73688de0982d6c96682088250447ed6d06214ba6
Jerome Bobin <48697119+jeromebob@users.noreply.github.com>
2022-08-08 00:41:46 +0200

    rajout du fondu du dot (chek or suppr ?)

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ window.addEventListener('load', () =>
     /*tu rajoute à .loader la class fondu-loader*/
     loader.classList.add('fondu-loader');
 )
+/*selection du dot via docqueryselector*/
+const dot = document.querySelector('dot');
+/*Quand tu auras chargé tout le site (load)*/
+window.addEventListener('load', () =>
+    /*tu rajoute à .loader la class fondu-loader*/
+    dot.classList.add('fondu-loader');
+)
