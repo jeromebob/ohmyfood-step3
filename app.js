4b0df951f0a5899db3ea83d8159f36b75d9c7851
Jerome Bobin <48697119+jeromebob@users.noreply.github.com>
2022-08-08 00:43:36 +0200

    suppr d'un ;

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const loader = document.querySelector('loader');
 /*Quand tu auras chargé tout le site (load)*/
 window.addEventListener('load', () =>
     /*tu rajoute à .loader la class fondu-loader*/
-    loader.classList.add('fondu-loader');
+    loader.classList.add('fondu-loader')
 )
+
 /*selection du dot via docqueryselector*/
 const dot = document.querySelector('dot');
 /*Quand tu auras chargé tout le site (load)*/
 window.addEventListener('load', () =>
     /*tu rajoute à .loader la class fondu-loader*/
-    dot.classList.add('fondu-loader');
+    dot.classList.add('fondu-loader')
 )
